8a8626467068ec0f1e449702332a0b3875521815
ggomez <gabrielgomez19@gmail.com>
2018-10-20 20:26:41 -0300

    Cambio en clase para ocultar elementos.

--- dist/js/cotizar.js
+++ dist/js/cotizar.js
@@ -2,43 +2,43 @@
  * 
  */
 $(document).ready(function(){
-	$('#botonEnviar').addClass('invisible');
+	$('#botonEnviar').addClass('d-none');
 	$('#campoDireccion').height(0);
 	$("#continuarpaso3").click(function(){
-		$('#botonSiguiente').removeClass('invisible');
+		$('#botonSiguiente').removeClass('d-none');
 	});
 	$('.titulosStep').click(function(){
 		tmp = parseInt($(this).attr('flag'));
 		if(parseInt($(this).attr('flag')))
-			$('#botonSiguiente').addClass('invisible');
+			$('#botonSiguiente').addClass('d-none');
 	});
 	$('.radios').click(function(){
 		if(!parseInt($(this).attr('pasos-req')))
 		{
-			$('#seleccionaProd').addClass('invisible');
-			$('#seleccionaCap').addClass('invisible');
+			$('#seleccionaProd').addClass('d-none');
+			$('#seleccionaCap').addClass('d-none');
 			$('#seleccionaProd').height(0);
 			$('#seleccionaCap').height(0);
-			$('#seleccionaCap').addClass('invisible');
+			$('#seleccionaCap').addClass('d-none');
 			$('#seleccionaProd').removeClass('step');
 			$('#seleccionaCap').removeClass('step');
-			$('#botonSiguiente').removeClass('invisible');
+			$('#botonSiguiente').removeClass('d-none');
 		}
 		else
 		{
 			$('#seleccionaProd').removeAttr('style');
 			$('#seleccionaCap').removeAttr('style');
-			$('#seleccionaProd').removeClass('invisible');
-			$('#seleccionaCap').removeClass('invisible');
+			$('#seleccionaProd').removeClass('d-none');
+			$('#seleccionaCap').removeClass('d-none');
 			$('#seleccionaProd').addClass('step');
 			$('#seleccionaCap').addClass('step');
-			$('#botonSiguiente').addClass('invisible');
+			$('#botonSiguiente').addClass('d-none');
 		}
 	});
 	
 	$('#botonSiguiente').click(function(){
-		$('#personaTag').removeClass('invisible');
-		$('#empresaTag').removeClass('invisible');
+		$('#personaTag').removeClass('d-none');
+		$('#empresaTag').removeClass('d-none');
 		$('#personaTag').children().addClass('active');
 		$('#empresaTag').children().removeClass('active');
 		$('.stepContent').removeClass('active');
@@ -48,17 +48,17 @@ $(document).ready(function(){
 		$('#stepTagArrow').removeClass('d-none');
 		$('#stepTagText').children().removeClass('active');
 		$('#stepTagArrow').children().removeClass('active');
-		$('#botonSiguiente').addClass('invisible');
-		$('#botonEnviar').removeClass('invisible');
+		$('#botonSiguiente').addClass('d-none');
+		$('#botonEnviar').removeClass('d-none');
 	});
 	
 	$('#stepTagArrow').click(function(){
-		$('#personaTag').addClass('invisible');
-		$('#empresaTag').addClass('invisible');
+		$('#personaTag').addClass('d-none');
+		$('#empresaTag').addClass('d-none');
 		$('#stepTagText').removeClass('d-none');
 		$('#stepTagArrow').addClass('d-none');
-		$('#botonSiguiente').removeClass('invisible');
-		$('#botonEnviar').addClass('invisible');
+		$('#botonSiguiente').removeClass('d-none');
+		$('#botonEnviar').addClass('d-none');
 	});
 	
 	$('#botonEnviar').click(function(){
@@ -76,11 +76,11 @@ $(document).ready(function(){
 		if(this.id == 'conInstalacion')
 		{
 			$('#campoDireccion').removeAttr('style');
-			$('#campoDireccion').removeClass('invisible');
+			$('#campoDireccion').removeClass('d-none');
 		}
 		else
 		{
-			$('#campoDireccion').addClass('invisible');
+			$('#campoDireccion').addClass('d-none');
 			$('#campoDireccion').height(0);
 		}
 	});
@@ -111,14 +111,14 @@ $(document).ready(function(){
 });
 
 verificaPasos = function(){
-	$('#personaTag').removeClass('invisible');
-	$('#empresaTag').removeClass('invisible');
+	$('#personaTag').removeClass('d-none');
+	$('#empresaTag').removeClass('d-none');
 	$('.stepContent').removeClass('active');
 	$('.stepContent').removeClass('show');
 	$('.empresaContent').removeClass('active');
 	$('.personaContent').removeClass('active');
 	$('.personaContent').addClass('active');
 	$('.personaContent').addClass('show');
-	$('#botonEnviar').removeClass('invisible');
+	$('#botonEnviar').removeClass('d-none');
 	$('.stepper').nextStep();
 }
