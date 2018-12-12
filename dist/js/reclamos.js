32fcdb7852c24de667901e58486cd08d03614ce0
ggomez <gabrielgomez19@gmail.com>
2018-12-11 22:23:22 -0300

    Cambios que incluyen registro de reclmos.

---
+++ dist/js/reclamos.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$("#enviarReclamo").click(function() {
+		reclamos($('#formReclamo')[0]);
+	});
+	$("#reclamos-checkbox").click(function(e) {
+		if(this.checked)
+		{
+			$('#enviarReclamo').removeClass('disabled');
+		}
+	});
+});
+reclamos = function(e){
+	var result = e.checkValidity();
+	if (e.checkValidity() === false)
+	{
+		e.classList.add('was-validated');
+	}
+	else
+	{
+		let data = new Object();
+		e.classList.add('was-validated');
+		form = new Array();
+		$.each(e.elements, function(k, v) {
+			let campos = new Object();
+			campos[v.name] = v.value;
+			form.push(campos);
+		});
+		data.formulario = form;
+		$.ajax(
+				{
+					method : 'POST',
+					url : 'http://declimamanager.declima.cl/reclamos/recibereclamo',
+					data : {
+						data : data
+					},
+					dataType : 'json'
+				})
+				.done(function(data, textStatus, jqXHR) {
+					let tmp = 0;
+				});
+	}
+}
--- email/ejecutivo-reclamo.php
+++ email/ejecutivo-reclamo.php
@@ -142,9 +142,8 @@ div[style*="margin: 14px 0;"] {
 										<img src="http://declima.cl/email-cotizacion/new-user.png"
 										width="100" height="100" style="display: block; border: 0px;" /><br>
 										<h2
-											style="font-size: 30px; font-weight: 600; line-height: 36px; color: #00587A; margin: 0;">
-											Nueva Cotizaci&oacute;n / <span>Persona</span>
-										</h2>
+											style="font-size: 30px; font-weight: 600; line-height: 36px; color: red; margin: 0;">
+											Reclamo Usuario</h2>
 									</td>
 								</tr>
 								<tr>
@@ -158,7 +157,7 @@ div[style*="margin: 14px 0;"] {
 											<tr>
 												<td width="65%" align="left" bgcolor="#fafafa"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 700; line-height: 24px; padding: 10px; border-top: 1px solid #eeeeee; color: #00587a;">
-													Nº de Solicitud:</td>
+													Nº de Reclamo</td>
 												<td width="35%" align="left" bgcolor="#fafafa"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 700; line-height: 24px; padding: 10px; border-top: 1px solid #eeeeee; color: #00587a;">
 													<span><?php echo $parametros['codigo']?></span>
@@ -167,70 +166,43 @@ div[style*="margin: 14px 0;"] {
 											<tr>
 												<td width="65%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Fecha de solicitud:</td>
+													Fecha de reclamo:</td>
 												<td width="35%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $parametros['fecha'];?></span>
+													<span><?php echo $parametros['fecha']?></span>
 												</td>
 											</tr>
 											<tr>
 												<td width="65%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Tipo de servicio:</td>
+													Nombre y Apellido:</td>
 												<td width="35%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $parametros['servicio'];?></span>
+													<span><?php echo $parametros['nombre']?></span>
 												</td>
 											</tr>
 											<tr>
 												<td width="65%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Producto:</td>
+													Email:</td>
 												<td width="35%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $parametros['producto'];?></span>
+													<span><?php echo $parametros['email']?></span>
 												</td>
 
-											</tr>
-
-											<?php foreach($parametros['capacidades'] as $key => $capacidad):?>
-											<tr>
-												<td width="65%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Capacidad (BTU):</td>
-												<td width="35%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $key?></span> x <span><?php echo $capacidad?></span>
-												</td>
-											</tr>
-											<?php endforeach;?>
-
-											<tr>
-												<td width="65%" align="left"
-													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Cantidad total:</td>
-												<td width="35%" align="left"
-													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $parametros['cantidadTotal']?></span> equipos
-												</td>
-
-											</tr>
-											<tr>
-												<td width="65%" align="left"
-													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Instalaci&oacute;n:</td>
-												<td width="35%" align="left"
-													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													S&iacute;</td>
 											</tr>
 
 											<tr>
 												<td width="65%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													Direcci&oacute;n de Instalaci&oacute;n:</td>
+													Asunto:</td>
 												<td width="35%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-													<span><?php echo $parametros['direccion']?></span>
+													<span><?php echo $parametros['asunto']?></span>
 												</td>
 											</tr>
+
+
 
 											<tr>
 												<td width="65%" align="left"
@@ -238,98 +210,13 @@ div[style*="margin: 14px 0;"] {
 													Comentario:</td>
 												<td width="35%" align="left"
 													style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px;">
-													<span><?php echo $parametros['comentario']?></span>
+													<span><?php echo $parametros['reclamo']?></span>
 												</td>
 											</tr>
 
 										</table>
 									</td>
 								</tr>
-
-								<!--OPEN USER PERSONA-->
-								<span>
-									<tr>
-										<td align="left" style="padding-top: 20px;">
-											<table cellspacing="0" cellpadding="0" bgcolor="#fafafa"
-												border="0" width="100%">
-												<tr>
-													<td width="65%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 700; line-height: 24px; padding: 10px; border-top: 1px solid #eeeeee; border-bottom: 1px solid #eeeeee; color: #00587a;">
-														Datos Usuarios (Persona):</td>
-													<td width="35%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 700; line-height: 24px; padding: 10px; border-top: 1px solid #eeeeee; border-bottom: 1px solid #eeeeee;"></td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-
-									<tr>
-										<td align="left">
-											<table cellspacing="0" cellpadding="0" border="0"
-												width="100%">
-												<tr>
-													<td width="65%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														Nombre y Apellido:</td>
-													<td width="35%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														<span><?php echo $parametros['nombre']?></span>
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-
-									<tr>
-										<td align="left">
-											<table cellspacing="0" cellpadding="0" border="0"
-												width="100%">
-												<tr>
-													<td width="65%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														Email:</td>
-													<td width="35%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														<span><?php echo $parametros['email']?></span>
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-
-									<tr>
-										<td align="left">
-											<table cellspacing="0" cellpadding="0" border="0"
-												width="100%">
-												<tr>
-													<td width="65%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														Tel&eacute;fono:</td>
-													<td width="35%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														<span><?php echo $parametros['telefono']?></span>
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-
-									<tr>
-										<td align="left">
-											<table cellspacing="0" cellpadding="0" border="0"
-												width="100%">
-												<tr>
-													<td width="65%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 600; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														Direcci&oacute;n:</td>
-													<td width="35%" align="left"
-														style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 10px; border-bottom: 1px solid #eeeeee;">
-														<span><?php echo $parametros['direccion']?></span>
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-								</span>
-								<!--END USER PERSONA-->
 
 							</table> <!--[if (gte mso 9)|(IE)]>
                 </td>
@@ -338,44 +225,6 @@ div[style*="margin: 14px 0;"] {
                 <![endif]-->
 						</td>
 					</tr>
-					<td align="center"
-						style="padding: 35px; background-color: #48a5f9;"
-						bgcolor="#48a5f9">
-						<!--[if (gte mso 9)|(IE)]>
-                <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
-                <tr>
-                <td align="center" valign="top" width="600">
-                <![endif]-->
-						<table align="center" border="0" cellpadding="0" cellspacing="0"
-							width="100%" style="max-width: 600px;">
-							<tr>
-								<td align="center"
-									style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;">
-									<h2
-										style="font-size: 24px; font-weight: 500; line-height: 30px; color: #ffffff; margin: 0;">
-										Administrar contactos desde la BBDD</h2>
-								</td>
-							</tr>
-							<tr>
-								<td align="center" style="padding: 25px 0 15px 0;">
-									<table border="0" cellspacing="0" cellpadding="0">
-										<tr>
-											<td align="center" style="border-radius: 5px;"
-												bgcolor="#00587A"><a
-												href="http://declima.cl/declimaManager/web/" target="_blank"
-												style="font-size: 18px; font-family: Open Sans, Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; border-radius: 5px; background-color: #00587A; padding: 15px 30px; display: block;">Ingresar</a>
-											</td>
-										</tr>
-									</table>
-								</td>
-							</tr>
-						</table> <!--[if (gte mso 9)|(IE)]>
-                </td>
-                </tr>
-                </table>
-                <![endif]-->
-					</td>
-
 					<tr>
 						<td align="center"
 							style="padding: 60px 35px 35px 35px; background-color: #fafafa; border-top: 1px solid #eeeeee;"
@@ -398,6 +247,37 @@ div[style*="margin: 14px 0;"] {
 										<p
 											style="font-size: 14px; font-weight: 700; line-height: 18px; color: #00587a;">
 											DECLIMA<br> Buenas Experiencias
+										</p>
+									</td>
+								</tr>
+								<tr>
+									<td align="left"
+										style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px;">
+										<p
+											style="font-size: 9px; font-weight: 400; line-height: 20px; color: #777777; text-align: justify;">
+
+											Declima – Ingeniería y Servicios en Climatización, Ofertas
+											válidas hasta agotar stock. El stock de unidades corresponde
+											al total disponible en declima.cl Ofertas aplican sobre
+											precio normal, descuentos ya aplicados a precio Internet.
+											Válido pagando con todo medio. Las ofertas no son canjeables
+											ni reembolsables por dinero en efectivo. Valor del despacho
+											no está incluido en el precio del producto. Las imágenes son
+											referenciales. Los valores incluyen IVA. Exclusivo
+											declima.cl. Declima © 2015 | Ingeniería y Servicios en
+											Climatización. Santiago de Chile. Todos los derechos
+											reservados.</p>
+									</td>
+								</tr>
+								<tr>
+									<td align="left"
+										style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px;">
+										<p
+											style="font-size: 9px; font-weight: 400; line-height: 20px; color: #777777;">
+
+											Si no creó una cuenta con esta dirección de correo
+											electrónico, ignore este correo electrónico o cancele su <a
+												href="" target="_blank" style="color: #777777;">suscripción.</a>.
 										</p>
 									</td>
 								</tr>
